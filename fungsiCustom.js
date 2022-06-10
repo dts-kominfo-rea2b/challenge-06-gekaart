58d254c56d29ddcbc5a9dde38eaa3a6288d569a8
gekaart <gekaarissta@gmail.com>
2022-06-10 20:32:58 +0700

    Answer Challenge 6

--- fungsiCustom.js
+++ fungsiCustom.js
@@ -1,4 +1,6 @@
+
 // TODO: import module bila dibutuhkan di sini
+const fs = require('fs');
 
 // ! JANGAN DIMODIFIKASI
 let file1 = "./data1.json";
@@ -18,7 +20,43 @@ let modifyFile3 = (val) => {
 
 // TODO: Kerjakan bacaData
 // gunakan variabel file1, file2, dan file3
-const bacaData = null;
+
+// 1. store json files in array `fileList`
+// 2. loop through fileList and do:
+// fs.readFile wrapped in Promised
+// 3. if error found, store the Error type
+// 4. otherwise, process and return data in processData(dataset)
+// 5. push data to result[]
+// 6. call fnCallback
+
+
+
+const bacaData = (fnCallback) => {
+  let result = []
+  fs.readFile(file1, 'utf8', (err, data)=>{
+    if(err){
+      fnCallback(err,null)
+    }
+    const  {message}  = JSON.parse(data);
+    result.push(message.split(" ")[1]);
+    fs.readFile(file2,'utf8', (err,data)=>{
+      if(err){
+        fnCallback(err,null)
+      }
+      const parseData = JSON.parse(data);
+      result.push(parseData[0].message.split(" ")[1])
+      fs.readFile(file3,'utf8',(err,data)=>{
+        if(err){
+          fnCallback(err,null)
+        }
+        const parseData = JSON.parse(data);
+        result.push(parseData[0].data.message.split(" ")[1])
+        fnCallback(null, result);
+      })
+    })
+  })
+
+}
 
 // ! JANGAN DIMODIFIKASI
 module.exports = {
